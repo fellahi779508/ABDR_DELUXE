b0316e0fb5c434c28848631a575a2fc3e86a0849
DuSStQ <fellahi574@gmail.com>
2025-09-17 17:51:04 +0100

    another bug fixed

--- frontend/src/app/(app)/cars/page.tsx
+++ frontend/src/app/(app)/cars/page.tsx
@@ -2,9 +2,12 @@ import CarBrowserComp from "@/components/browser-page/car-browser.component";
 import styles from "@/styles/car-browser.module.css";
 import { FetchAllBrands, GetAllVisibleCars } from "@/utils/Admin";
 
+export const dynamic = "force-dynamic";
+
 async function CarBrowser() {
 	const cars = await GetAllVisibleCars();
 	const brands = await FetchAllBrands();
+
 	return (
 		<div className={styles.container}>
 			<CarBrowserComp car={cars} brands={brands} />
